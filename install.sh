6a599cfcc676dfae8730cc46417eb37204f8bf6b
Guillaume Valadon <guillaume@valadon.net>
2016-04-01 13:28:59 +0200

    Install some modules with pip

---
+++ install.sh
@@ -0,0 +1,4 @@
+if [ ! -z $DO_PCAPDNET ]
+then
+  pip install pcapy dnet
+fi
--- test.py
+++ test.py
@@ -4,3 +4,15 @@ import sys
 print sys.version
 print sys.platform
 print os.getuid()
+
+try:
+    import pcapy
+    print "pcapy is installed"
+except:
+    pass
+
+try:
+    import dnet
+    print "dnet is installed"
+except:
+    pass
