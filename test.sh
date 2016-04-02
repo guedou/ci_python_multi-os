00140fd65a5a6e1cece601cf485fc93038b67c0e
Guillaume Valadon <guillaume@valadon.net>
2016-04-02 20:03:49 +0200

    export homebrew PYTHONPATH

--- test.sh
+++ test.sh
@@ -2,6 +2,10 @@ if [ -z $CAN_SUDO ]
 then
   python test.py
 else
+  if [ "$TRAVIS_OS_NAME" = "osx" ]
+  then
+    export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
+  fi
   sudo python test.py
   ping -c 1 8.8.8.8
 fi
