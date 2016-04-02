2a795c8ed4a091965e30cfa4f1ae17cc37cc3223
Guillaume Valadon <guillaume@valadon.net>
2016-04-02 19:38:20 +0200

    Better import messages

--- test.py
+++ test.py
@@ -9,10 +9,12 @@ try:
     import pcapy
     print "pcapy is installed"
 except:
+    print "pcapy is NOT installed"
     pass
 
 try:
     import dnet
     print "dnet is installed"
 except:
+    print "dnet is NOT installed"
     pass
